142415be2cfdac4b1ddea9877ce16f5b9bc35418
Ben Evans <benjamin.robert.evans@gmail.com>
2013-10-16 13:33:09 -0400

    fixed data types in scrub_pop_assignment

--- scrub_pop_assignment.py
+++ scrub_pop_assignment.py
@@ -7,27 +7,26 @@ import brewer2mpl
 
 def struct_to_fig(data):
     for a in data:
-        K = int(a['assn'].shape[0])
-        bmap = brewer2mpl.get_map('Accent', 'qualitative', K)
+        K = int(data[a]['assn'].shape[0])
+        bmap = brewer2mpl.get_map('Set3', 'qualitative', K)
         colors = bmap.mpl_colors
         width=1
-        xcoord = np.arange(len(a['ids'])*width, step=width)
+        xcoord = np.arange(len(data[a]['ids'])*width, step=width)
         bars=[]
 
-        xcoord = np.arange(len(a['ids']))
-        for i, (c, ki) in enumerate(zip(colors,a['assn'])):
-            print c
+        xcoord = np.arange(len(data[a]['ids'])*width, step=width)
+        for i, (c, ki) in enumerate(zip(colors,data[a]['assn'])):
             if i ==0:
                 bars.append( plt.bar(left=xcoord, height=ki, width=width, color=c, edgecolor='none') )
             else:
-                bottom = np.sum(a['assn'][:i,:], axis=0)
+                bottom = np.sum(data[a]['assn'][:i,:], axis=0)
                 bars.append( plt.bar(left=xcoord, height=ki, width=width, color=c, bottom=bottom, edgecolor='none') )
 
         plt.title('Structure Plot, K='+str(K))
         plt.grid(False)
         plt.xlim([0,xcoord[-1]+width])
         plt.ylim([0,1])
-        plt.xticks(xcoord+width/2., a['ids'], rotation=90, fontsize=6)
+        plt.xticks(xcoord+width/2., data[a]['ids'], rotation=90, fontsize=6)
         plt.tick_params(axis='x', which='both', bottom='on', top='off', direction='out')
         plt.tick_params(axis='y', which='both', left='off', right='off', labelleft='off')
 
@@ -36,7 +35,7 @@ def struct_to_fig(data):
 
 data = {}
 for struct_file in iglob(getcwd()+'/*_f'):
-        out = open(struct_file[:-2]+'_cleaned.txt', 'w')
+        out = open(struct_file[:-2]+'_indiv_q.txt', 'w')
         started = False
         ids = []
         assignments = []
@@ -52,9 +51,9 @@ for struct_file in iglob(getcwd()+'/*_f'):
             if started:
                 tmp = l.split()
                 ids.append(tmp[1])
-                assignments.append(tmp[4:])
+                assignments.append(tmp[5:])
                 out.write('\t'.join(tmp[0:4])+'\t'+'\t'.join(tmp[5:])+'\n')
-        data[struct_file[:-2]] = {'ids':ids, 'assn':np.array(assignments).T}
+        data[struct_file[:-2]] = {'ids':ids, 'assn':np.array(assignments, dtype='float').T}
 
 struct_to_fig(data)
 
